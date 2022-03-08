a969790cb88a7f665968a58af757ed23c3fcd4d8
amer <amer009966@gmail.com>
2022-03-08 00:00:09 -0500

    gitflow-feature-stash: pag_productos

---
+++ resources/views/products.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.layout')
+
+@section('content')
+
+    <!--=====================================
+    Breadcrumb
+    ======================================-->  
+	
+	<div class="ps-breadcrumb">
+
+        <div class="container">
+
+            <ul class="breadcrumb">
+
+                <li><a href="index.html">Home</a></li>
+
+                <li><a href="shop-default.html">Garden &amp; Kitchen</a></li>
+
+                <li><a href="shop-default.html">Furniture</a></li>
+
+                <li>Korea Long Sofa Fabric In Blue Navy Color</li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+
+@endsection
